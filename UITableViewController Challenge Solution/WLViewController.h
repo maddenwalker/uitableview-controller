2763047d0420c793e4d2768b5cd768fab7ef7bfd
Ryan Walker <ryan@maddenwalker.com>
2014-09-11 17:53:29 -0700

    Initial Commit

---
+++ UITableViewController Challenge Solution/WLViewController.h
@@ -0,0 +1,13 @@
+//
+//  WLViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Ryan Walker on 9/11/14.
+//  Copyright (c) 2014 Witt Labs, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLViewController : UIViewController
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Ryan Walker on 9/11/14.
+//  Copyright (c) 2014 Witt Labs, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WLAppDelegate class]));
+    }
+}
